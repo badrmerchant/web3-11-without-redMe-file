f8e8a6f62c589a2edd310268f8003f4770b33753
BadarAhmedSheikh <badr_merchant@yahoo.com>
2023-02-23 15:46:09 +0500

    chlak42-2

--- chhina.js
+++ chhina.js
@@ -3,4 +3,7 @@ console.log("chhina");
 
 function add(){console.log("chlak42");}
 
-function add111(){console.log("chalk42");}
+function add111(){console.log("chalk42-1");}
+
+
+function add222(){console.log("chalk42-2");}
